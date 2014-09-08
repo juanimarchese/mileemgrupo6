7c3e3830f12d735648df2b9463922a5993bd0f2f
Ramiro Diaz <ramiro.diaz@avature.net>
2014-09-08 16:57:18 -0300

    us #206 / task #337 - Agrego cliente rest, modelo idname y data manager.

---
+++ app/src/main/java/com/mileem/mileem/AsyncRestHttpClient.java
@@ -0,0 +1,7 @@
+package com.mileem.mileem;
+
+/**
+ * Created by ramirodiaz on 07/09/14.
+ */
+public class AsyncRestHttpClient {
+}
---
+++ app/src/main/java/com/mileem/mileem/IdName.java
@@ -0,0 +1,7 @@
+package com.mileem.mileem;
+
+/**
+ * Created by ramirodiaz on 08/09/14.
+ */
+public class IdName {
+}
