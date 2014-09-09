611b310a12bf4442bd7415a3a80d6b234d7a2bd0
Ramiro Diaz <ramiro.diaz@avature.net>
2014-09-09 00:36:35 -0300

    us #206 / task #337 - Implementacion del manager de definiciones

--- app/src/main/java/com/mileem/mileem/DefinitionsManager.java
+++ app/src/main/java/com/mileem/mileem/DefinitionsManager.java
@@ -1,12 +1,20 @@
 package com.mileem.mileem;
 
+import java.util.Collection;
+
 /**
  * Created by ramirodiaz on 09/09/14.
  */
 
 public class DefinitionsManager {
     private static volatile DefinitionsManager instance = null;
+
+    private Collection<IdName> neightboardsCollection = null;
+    private Collection<IdName> environmentsTypesCollection = null;
+    private Collection<IdName> operationTypesCollection = null;
+    private Collection<IdName> propertyTypesCollection = null;
     private DefinitionsManager() {};
+
     public static DefinitionsManager getInstance() {
         if (instance == null) {
             synchronized (DefinitionsManager.class) {
@@ -14,7 +22,40 @@ public class DefinitionsManager {
                     instance = new DefinitionsManager();
                 }
             }
+
         }
         return instance;
+    }
+
+    public Collection<IdName> getNeightboardsCollection() {
+        return neightboardsCollection;
+    }
+
+    public void setNeightboardsCollection(Collection<IdName> neightboardsCollection) {
+        this.neightboardsCollection = neightboardsCollection;
+    }
+
+    public Collection<IdName> getEnvironmentsTypesCollection() {
+        return environmentsTypesCollection;
+    }
+
+    public void setEnvironmentsTypesCollection(Collection<IdName> environmentsTypesCollection) {
+        this.environmentsTypesCollection = environmentsTypesCollection;
+    }
+
+    public Collection<IdName> getOperationTypesCollection() {
+        return operationTypesCollection;
+    }
+
+    public void setOperationTypesCollection(Collection<IdName> operationTypesCollection) {
+        this.operationTypesCollection = operationTypesCollection;
+    }
+
+    public Collection<IdName> getPropertyTypesCollection() {
+        return propertyTypesCollection;
+    }
+
+    public void setPropertyTypesCollection(Collection<IdName> propertyTypesCollection) {
+        this.propertyTypesCollection = propertyTypesCollection;
     }
 }
