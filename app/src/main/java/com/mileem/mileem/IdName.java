d6b2fbbd9cf82bb0c9a35dddf869d78d293f264d
Ramiro Diaz <ramiro.diaz@avature.net>
2014-09-09 00:10:02 -0300

    us #206 / task #337 - Implementacion name id model

--- app/src/main/java/com/mileem/mileem/IdName.java
+++ app/src/main/java/com/mileem/mileem/IdName.java
@@ -1,7 +1,24 @@
 package com.mileem.mileem;
 
+import com.google.gson.Gson;
+
 /**
  * Created by ramirodiaz on 08/09/14.
  */
 public class IdName {
+    String name;
+    Number id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public IdName(String name, Number id) {
+        this.name = name;
+        this.id = id;
+    }
 }
