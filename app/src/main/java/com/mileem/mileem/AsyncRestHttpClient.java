33ed4f29e0f7c72100c7b1aa43ff4c21ee3955b3
Ramiro Diaz <ramiro.diaz@avature.net>
2014-09-09 00:14:53 -0300

    us #206 / task #337 - Agrego implementacion de cliente rest

--- app/src/main/java/com/mileem/mileem/AsyncRestHttpClient.java
+++ app/src/main/java/com/mileem/mileem/AsyncRestHttpClient.java
@@ -1,7 +1,26 @@
 package com.mileem.mileem;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 /**
  * Created by ramirodiaz on 07/09/14.
  */
+
 public class AsyncRestHttpClient {
+    private static final String BASE_URL = "http://www.mocky.io/v2/";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
 }
