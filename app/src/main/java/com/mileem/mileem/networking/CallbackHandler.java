c69eb690ee7de35cf99897b2d4ad712a7674fb1e
Ramiro Diaz <ramiro.diaz@avature.net>
2014-09-12 02:24:19 -0300

    us #206 / TASK #410 - Agrego nuevo callback

--- app/src/main/java/com/mileem/mileem/networking/CallbackHandler.java
+++ app/src/main/java/com/mileem/mileem/networking/CallbackHandler.java
@@ -1,10 +1,13 @@
 package com.mileem.mileem.networking;
 
+import java.util.Collection;
+
 /**
  * Created by ramirodiaz on 09/09/14.
  */
 public abstract class CallbackHandler {
     public abstract void onComplete(Object object);
+    public abstract void onComplete(Collection collection);
     public abstract void onFailure(Object object);
 }
 
