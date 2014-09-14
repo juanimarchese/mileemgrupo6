83af92f2da0cda4404a73fc5e6bf2bb6dbfc18f0
Juan Marchese <juanimarchese@gmail.com>
2014-09-13 23:44:28 -0300

    us #206 / Cambio de textbox por spinner para ambientes

--- app/src/main/java/com/mileem/mileem/fragments/SearchFragment.java
+++ app/src/main/java/com/mileem/mileem/fragments/SearchFragment.java
@@ -4,18 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
+import android.widget.*;
 import com.mileem.mileem.R;
 import com.mileem.mileem.activities.MainActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class SearchFragment extends BaseFragment {
 
     public static final String TAG = SearchFragment.class.getSimpleName();
 
-    private AutoCompleteTextView actv;
+    private AutoCompleteTextView barrioACTV;
+    private Spinner tipoPropiedadSpinner;
+    private Spinner operacionSpinner;
+    private Spinner ambientesSpinner;
 
     public SearchFragment() {
         super(TAG);
@@ -25,15 +30,47 @@ public class SearchFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.search_fragment, container, false);
-        String[] countries = getResources().
-                getStringArray(R.array.barrios);
-        ArrayAdapter adapter = new ArrayAdapter(rootView.getContext(),android.R.layout.select_dialog_item,countries);
-        actv = (AutoCompleteTextView) rootView.findViewById(R.id.barrio);
-        actv.setThreshold(1);//will start working from first character
-        actv.setAdapter(adapter);
+        addItemsToAutoCompleteTextView(rootView);
+        addItemsToTipoPropiedadSpinner(rootView);
+        addItemsToOperacionSpinner(rootView);
+        addItemsToAmbientesSpinner(rootView);
         return rootView;
     }
 
+    private void addItemsToAmbientesSpinner(View rootView) {
+        tipoPropiedadSpinner = (Spinner) rootView.findViewById(R.id.tipo_propiedad);
+        List list = new ArrayList();
+        Collections.addAll(list, getResources().getStringArray(R.array.tipos_propiedad));
+        ArrayAdapter dataAdapter = new ArrayAdapter(rootView.getContext(),android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tipoPropiedadSpinner.setAdapter(dataAdapter);
+    }
+
+    private void addItemsToOperacionSpinner(View rootView) {
+        ambientesSpinner = (Spinner) rootView.findViewById(R.id.operacion);
+        List list = new ArrayList();
+        Collections.addAll(list, getResources().getStringArray(R.array.operaciones));
+        ArrayAdapter dataAdapter = new ArrayAdapter(rootView.getContext(),android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ambientesSpinner.setAdapter(dataAdapter);
+    }
+
+    private void addItemsToTipoPropiedadSpinner(View rootView) {
+        operacionSpinner = (Spinner) rootView.findViewById(R.id.ambientes);
+        List list = new ArrayList();
+        Collections.addAll(list, getResources().getStringArray(R.array.ambientes));
+        ArrayAdapter dataAdapter = new ArrayAdapter(rootView.getContext(),android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        operacionSpinner.setAdapter(dataAdapter);
+    }
+
+    private void addItemsToAutoCompleteTextView(View rootView) {
+        String[] countries = getResources().getStringArray(R.array.barrios);
+        ArrayAdapter adapter = new ArrayAdapter(rootView.getContext(),android.R.layout.select_dialog_item,countries);
+        barrioACTV = (AutoCompleteTextView) rootView.findViewById(R.id.barrio);
+        barrioACTV.setThreshold(1);//will start working from first character
+        barrioACTV.setAdapter(adapter);
+    }
 
 
     @Override
