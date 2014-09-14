cc972ab6aa0382ba921e968d998b41495c6c02a6
Ramiro Diaz <ramiro.diaz@avature.net>
2014-09-14 02:08:52 -0300

    us #206 / task #410 - Agrego autenticación para url de produ.

--- app/src/main/java/com/mileem/mileem/networking/AsyncRestHttpClient.java
+++ app/src/main/java/com/mileem/mileem/networking/AsyncRestHttpClient.java
@@ -4,19 +4,28 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import java.util.Map;
+
 /**
  * Created by ramirodiaz on 07/09/14.
  */
 
 public class AsyncRestHttpClient {
-    private static final String BASE_URL = "http://www.mocky.io/v2/";
+    private static final String BASE_URL_PROD = "http://107.20.253.197/api/";
+    //TODO hacer andar para simulador contra localhost
+    private static final String BASE_URL_DEV = "http://107.20.253.197/api/";
+    private static final String BASE_URL = BASE_URL_PROD;
     private static AsyncHttpClient client = new AsyncHttpClient();
 
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        if (BASE_URL == BASE_URL_PROD)
+            client.setBasicAuth("mileen", "m1l33n");
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
     public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        if (BASE_URL == BASE_URL_PROD)
+            client.setBasicAuth("mileen", "m1l33n");
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
@@ -24,3 +33,5 @@ public class AsyncRestHttpClient {
         return BASE_URL + relativeUrl;
     }
 }
+
+
