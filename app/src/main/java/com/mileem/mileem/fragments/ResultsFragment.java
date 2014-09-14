df5b8bc0c1065370b56ae3e46ca0bff0963803cb
Juan Marchese <juanimarchese@gmail.com>
2014-09-13 22:20:52 -0300

    us #206 / TASK#434  Paginado (UI)

--- app/src/main/java/com/mileem/mileem/fragments/ResultsFragment.java
+++ app/src/main/java/com/mileem/mileem/fragments/ResultsFragment.java
@@ -2,6 +2,7 @@ package com.mileem.mileem.fragments;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.*;
@@ -18,26 +19,29 @@ import com.mileem.mileem.R;
 import com.mileem.mileem.activities.MainActivity;
 import com.mileem.mileem.adapters.PublicationListAdapter;
 import com.mileem.mileem.models.PublicationDetails;
+import com.mileem.mileem.networking.MockDataManager;
+import com.mileem.mileem.widgets.EndlessListView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Juan-Asus on 09/09/2014.
  */
-public class ResultsFragment extends BaseFragment {
+public class ResultsFragment extends BaseFragment implements EndlessListView.EndLessListener{
 
     public static final String TAG = ResultsFragment.class.getSimpleName();
 
-    private static final String url = "http://10.0.2.2/api/property-search";    //TODO - Cambiar a la url donde vienen los datos
     private ProgressDialog pDialog;
     private ArrayList<PublicationDetails> publicationList = new ArrayList<PublicationDetails>();
-    private ListView listView;
+    private EndlessListView listView;
     private PublicationListAdapter adapter;
+    private Context context;
 
-    private int cant = 1;
+    private int pagina = 0;
 
     public ResultsFragment() {
         super(TAG);
@@ -53,7 +57,10 @@ public class ResultsFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.results_fragment, container, false);
-        listView = (ListView) rootView.findViewById(R.id.publicationList);
+        context = rootView.getContext();
+        listView = (EndlessListView) rootView.findViewById(R.id.publicationList);
+        listView.setLoadingView(R.layout.loading_layout);
+        listView.setListener(this);
         adapter = new PublicationListAdapter(publicationList,rootView.getContext());
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -62,93 +69,28 @@ public class ResultsFragment extends BaseFragment {
                 Toast.makeText(view.getContext(), "Vista de Detalles", Toast.LENGTH_LONG).show();
             }
         });
-        requestData(rootView);
+
+
+        requestFirstPageData();
         return rootView;
     }
 
-    private void requestData(final View rootView) {
-        final Context context = rootView.getContext();
-        pDialog = new ProgressDialog(context);
-        // Showing progress dialog before making http request
-        pDialog.setMessage("Buscando...");
-        pDialog.show();
+    private void requestFirstPageData() {
+        showPDialog();
+        pagina = 0;
         publicationList.clear();
-
-       /* for(int i=0; i<10;i++){
-        PublicationDetails publicacion = new PublicationDetails();
-        publicacion.setPrecio("U$D99999999");
-        publicacion.setThumbnailUrl("http://avatarbox.net/avatars/img19/zhou_ming_avatar_picture_49967.jpg");
-        publicacion.setDireccion("Av Consejal Tribulato 187" + i );
-        publicacion.setM2("1000 m2");
-        publicacion.setCantAmbientes("12 amb");
-
-        // adding publicacion to array
-        publicationList.add(publicacion);
-        }
-        adapter.notifyDataSetChanged();
-        hidePDialog();*/
-
-        // Creating volley request obj
-        JsonObjectRequest request = new JsonObjectRequest(url,null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d(TAG, response.toString());
-                        hidePDialog();
-
-                        try {
-                            JSONArray array = response.getJSONArray("payload");
-                            // Parsing json
-                            if(array.length() <= 0){
-                                ((MainActivity)rootView.getContext()).displayView(new NoResultsFragment());
-                            }   else {
-                                for (int i = 0; i < array.length(); i++) {
-
-
-                                    JSONObject obj = array.getJSONObject(i);
-
-                                    PublicationDetails publicacion = new PublicationDetails();
-                                    publicacion.setPrecio(obj.getString("currency")+ " " + obj.getString("price"));
-                                    publicacion.setThumbnailUrl("http://avatarbox.net/avatars/img19/zhou_ming_avatar_picture_49967.jpg");
-                                    publicacion.setDireccion(obj.getString("address"));
-                                    publicacion.setM2((obj.getInt("size") + obj.getInt("coveredSize"))+" m2");
-                                    publicacion.setCantAmbientes(obj.getJSONObject("environment").getString("name"));
-
-                                       // adding publicacion to array
-                                        publicationList.add(publicacion);
-
-
-
-                                }
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        // notifying list adapter about data changes
-                        // so that it renders the list view with updated data
-                        adapter.notifyDataSetChanged();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: " + error.getMessage());
-                Toast.makeText(getActivity(), "Error al tratar de obtener los datos", Toast.LENGTH_LONG).show();
-                ((MainActivity) context).displayViewForMenu(0);
-                hidePDialog();
-
-            }
-        });
-
-        // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(request);
+        requestData();
     }
+
+
+
 
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
         if(!hidden){
             //Checkear si cambiaron los criterios de busqueda, filtro u ordenamiento tambien, para no hacer busquedas sin sentido!
-            requestData(this.getView());
+            requestFirstPageData();
         }
     }
 
@@ -178,6 +120,13 @@ public class ResultsFragment extends BaseFragment {
         hidePDialog();
     }
 
+    private void showPDialog() {
+        pDialog = new ProgressDialog(context);
+        // Showing progress dialog before making http request
+        pDialog.setMessage("Buscando...");
+        pDialog.show();
+    }
+
     private void hidePDialog() {
         if (pDialog != null) {
             pDialog.dismiss();
@@ -185,4 +134,36 @@ public class ResultsFragment extends BaseFragment {
         }
     }
 
+    @Override
+    public void loadData() {
+        pagina += 1;
+        //	new data loader
+        requestData();
+    }
+
+
+    private void requestData() {
+        new FakeLoader().execute();
+    }
+
+    private class FakeLoader extends AsyncTask<Void,Void,List<PublicationDetails>> {
+
+        @Override
+        protected List<PublicationDetails> doInBackground(Void... params) {
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return MockDataManager.getPublicationsByPage(pagina);
+        }
+
+        @Override
+        protected void onPostExecute(List<PublicationDetails> result) {
+            super.onPostExecute(result);
+            listView.addNewData(result);
+            hidePDialog();
+        }
+    }
 }
---
+++ app/src/main/java/com/mileem/mileem/networking/MockDataManager.java
@@ -0,0 +1,84 @@
+package com.mileem.mileem.networking;
+
+import com.mileem.mileem.models.PublicationDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Juan-Asus on 13/09/2014.
+ */
+public class MockDataManager {
+    private static final String url = "http://10.0.2.2/api/property-search";
+
+    static public List<PublicationDetails> getPublicationsByPage(int pageNumber){
+        List publicationList = new ArrayList<PublicationDetails>();
+        for(int i=0+((pageNumber)*5); i<5*(pageNumber+1);i++){
+            PublicationDetails publicacion = new PublicationDetails();
+            publicacion.setPrecio("U$D99999999");
+            publicacion.setThumbnailUrl("http://avatarbox.net/avatars/img19/zhou_ming_avatar_picture_49967.jpg");
+            publicacion.setDireccion("Av Consejal Tribulato " + i );
+            publicacion.setM2("1000 m2");
+            publicacion.setCantAmbientes("12 amb");
+
+            // adding publicacion to array
+
+            publicationList.add(publicacion);
+        }
+
+                /* // Creating volley request obj
+        JsonObjectRequest request = new JsonObjectRequest(url,null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+
+
+                        try {
+                            JSONArray array = response.getJSONArray("payload");
+                            // Parsing json
+                            if(array.length() <= 0){
+                                ((MainActivity)context).displayView(new NoResultsFragment());
+                            }   else {
+                                for (int i = 0; i < array.length(); i++) {
+
+
+                                    JSONObject obj = array.getJSONObject(i);
+
+                                    PublicationDetails publicacion = new PublicationDetails();
+                                    publicacion.setPrecio(obj.getString("currency")+ " " + obj.getString("price"));
+                                    publicacion.setThumbnailUrl("http://avatarbox.net/avatars/img19/zhou_ming_avatar_picture_49967.jpg");
+                                    publicacion.setDireccion(obj.getString("address"));
+                                    publicacion.setM2((obj.getInt("size") + obj.getInt("coveredSize"))+" m2");
+                                    publicacion.setCantAmbientes(obj.getJSONObject("environment").getString("name"));
+
+                                       // adding publicacion to array
+                                        publicationList.add(publicacion);
+
+
+
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        notifySuccesDataRequest();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getActivity(), "Error al tratar de obtener los datos", Toast.LENGTH_LONG).show();
+                ((MainActivity) context).displayViewForMenu(0);
+                notifyFailDataRequest();
+
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(request);*/
+        return publicationList;
+    }
+}
