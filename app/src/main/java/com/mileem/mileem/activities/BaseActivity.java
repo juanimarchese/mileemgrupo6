1b59d9f5151d8d7233746909c776d5fb0a62ced4
Ramiro Diaz <ramiro.diaz@avature.net>
2014-09-15 00:33:38 -0300

    us #206 / task #333 - Ocultar keyboard antes de cambiar de fragment.

--- app/src/main/java/com/mileem/mileem/activities/BaseActivity.java
+++ app/src/main/java/com/mileem/mileem/activities/BaseActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.inputmethod.InputMethodManager;
+
 import com.mileem.mileem.R;
 import com.mileem.mileem.fragments.BaseFragment;
 
@@ -31,9 +34,14 @@ public abstract class BaseActivity extends Activity {
             if (currentFragment != null && currentFragment.getCustomTag().equals(fragment.getCustomTag()))
                 return;
 
-            if (currentFragment != null)
-                fragmentTransaction.hide(fm.findFragmentByTag(currentFragment.getCustomTag()));
+            if (currentFragment != null) {
 
+                Fragment f = fm.findFragmentByTag(currentFragment.getCustomTag());
+                final InputMethodManager imm = (InputMethodManager) f.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(f.getView().getWindowToken(), 0);
+
+                fragmentTransaction.hide(fm.findFragmentByTag(currentFragment.getCustomTag()));
+            }
             if (fragmentByTag == null) {
                fragmentTransaction.add(R.id.frame_container, fragment, fragment.getCustomTag());
             } else {
