539748abf48f08c294e639e936b73224c2282b88
Ramiro Diaz <ramiro.diaz@avature.net>
2014-09-20 18:47:52 -0300

    Us: 215 - Task: 295 - User Model. Primer enfoque.

---
+++ app/src/main/java/com/mileem/mileem/models/User.java
@@ -0,0 +1,34 @@
+package com.mileem.mileem.models;
+
+/**
+ * Created by ramirodiaz on 20/09/14.
+ */
+public class User {
+    int id;
+    String phone;
+    String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
