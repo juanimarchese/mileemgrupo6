3d3602271ae3f71a627cc3b8a1c80d2a5e36b1c7
Juan Marchese <juanimarchese@gmail.com>
2014-09-21 15:04:00 -0300

    us #215 / TASK 	299	 Mostrar tipo de inmueble/ 300	Mostrar ubicación. / 301	 Mostrar superficie./302  Mostrar cantidad de ambientes /303 Mostrar precio./304 Mostrar cantidad de expensas./ 305 Mostrar antiguedad./ 306 Mostrar descripción adicional.

--- app/src/main/java/com/mileem/mileem/fragments/PublicationDetailFragment.java
+++ app/src/main/java/com/mileem/mileem/fragments/PublicationDetailFragment.java
@@ -1,7 +1,5 @@
 package com.mileem.mileem.fragments;
 
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -9,6 +7,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mileem.mileem.R;
@@ -22,7 +21,7 @@ import com.mileem.mileem.networking.PublicationDetailsDataManager;
 public class PublicationDetailFragment extends BaseFragment {
 
     public static final String TAG = PublicationDetailFragment.class.getSimpleName();
-    private Context context;
+    private View rootView;
 
     public PublicationDetailFragment() {
         super(TAG);
@@ -60,24 +59,47 @@ public class PublicationDetailFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.publication_detail_fragment, container, false);
-        context = rootView.getContext();
-        buildPublicationView(rootView);
+        this.rootView = rootView;
+        buildPublicationView();
         requestPublicationData();
         return rootView;
     }
 
-    private void buildPublicationView(View rootView) {
+
+    private void buildPublicationView() {
         //TODO - Inicializar el Layout de Vista
 
     }
+    private void fillPublicationView(PublicationDetails publication) {
+        buildTextViewWidget(R.id.prop_direccion,publication.getAddress());
+        buildTextViewWidget(R.id.prop_barrio,publication.getNeighborhood().getName());
+        buildTextViewWidget(R.id.prop_precio,publication.getCurrency() + " " + publication.getPrice());
+        buildTextViewWidget(R.id.prop_m2, String.valueOf(publication.getCoveredSize() + publication.getSize())+ " m2");
+        buildTextViewWidget(R.id.prop_ambientes,publication.getEnvironment().getName());
+        buildTextViewWidget(R.id.prop_tipo,publication.getPropertyType().getName());
+        buildTextViewWidget(R.id.prop_op,publication.getOperationType().getName());
+        buildTextViewWidget(R.id.prop_scubierta,publication.getCoveredSize() + " m2");
+        buildTextViewWidget(R.id.prop_sdescubierta,publication.getSize() + " m2");
+        String antiguedad = publication.getAge() == 0 ? "A estrenar" : publication.getAge() > 1 ? publication.getAge() + " años" : "1 año";
+        buildTextViewWidget(R.id.prop_antiguedad, antiguedad);
+        buildTextViewWidget(R.id.prop_expensas, publication.getCurrency() + " " + publication.getExpenses());
+        buildTextViewWidget(R.id.prop_descripcion, publication.getDescription());
+    }
+
+    private void buildTextViewWidget(int convertViewId, String txt) {
+        TextView textView = (TextView)rootView.findViewById(convertViewId);
+        if(textView != null) textView.setText(txt);
+    }
+
 
     private void requestPublicationData() {
-        showPDialog(context);
+        showPDialog(rootView.getContext());
         try {
             new PublicationDetailsDataManager().getDetails(getPublicationId(), new PublicationDetailsDataManager.PublicationsDetailsCallbackHandler() {
                 @Override
                 public void onComplete(PublicationDetails publication) {
                     //TODO - Bindear datos con la vista
+                    fillPublicationView(publication);
                     hidePDialog();
                 }
 
@@ -95,7 +117,7 @@ public class PublicationDetailFragment extends BaseFragment {
 
     private void showError() {
         Toast.makeText(getActivity(), "Error al tratar de obtener los datos de la publicación", Toast.LENGTH_LONG).show();
-        ((MainActivity) context).displayViewForMenu(1);
+        ((MainActivity) rootView.getContext()).displayViewForMenu(1);
     }
 
     @Override
@@ -111,13 +133,13 @@ public class PublicationDetailFragment extends BaseFragment {
         // Handle presses on the action bar items
         switch (item.getItemId()) {
             case R.id.action_contact:
-                Toast.makeText(context, "Vista de Contacto", Toast.LENGTH_LONG).show();
+                Toast.makeText(rootView.getContext(), "Vista de Contacto", Toast.LENGTH_LONG).show();
                 return true;
             case R.id.action_amenities:
-                Toast.makeText(context, "Vista de Amenities", Toast.LENGTH_LONG).show();
+                Toast.makeText(rootView.getContext(), "Vista de Amenities", Toast.LENGTH_LONG).show();
                 return true;
             case R.id.action_location:
-                Toast.makeText(context, "Vista de Mapa", Toast.LENGTH_LONG).show();
+                Toast.makeText(rootView.getContext(), "Vista de Mapa", Toast.LENGTH_LONG).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
