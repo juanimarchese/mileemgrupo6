ab4a26c347d6488df6fc68078fe02af9ef93d725
Ramiro Diaz <ramiro.diaz@avature.net>
2014-10-26 15:48:03 -0300

    US 220 - T243 - Conectividad para obtener el gráfico.

---
+++ app/src/main/java/com/mileem/mileem/networking/ReportDataManager.java
@@ -0,0 +1,40 @@
+package com.mileem.mileem.networking;
+
+/**
+ * Created by ramirodiaz on 26/10/14.
+ */
+
+import com.google.gson.Gson;
+import com.loopj.android.http.RequestParams;
+import com.mileem.mileem.models.PublicationDetails;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ReportDataManager {
+    public static abstract class ReportDataManagerCallbackHandler extends CallbackHandler {
+        public abstract void onComplete(String neighborhoodName, String graphUrl);
+    }
+    public void getReportAveragePricePerSquareMeterOfSurroundingNeighborhood(final int neighborhoodId, final int width, final int height, final ReportDataManagerCallbackHandler callbackHandler) throws JSONException {
+        RequestParams params = new RequestParams();
+        params.put("neighborhood", neighborhoodId);
+        params.put("width", width);
+        params.put("height", height);
+        AsyncRestHttpClient.get("average-price-by-neighborhood", params, new MileenJsonResponseHandler(callbackHandler) {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    JSONObject payload = response.getJSONObject("payload");
+                    String relativeUrl = payload.getString("url");
+                    String neighborhoodName = payload.getJSONObject("neighborhood").getString("name");
+                    String graphUrl = AsyncRestHttpClient.getAbsoluteUrlRelativeToHost(relativeUrl);
+                    callbackHandler.onComplete(neighborhoodName, graphUrl);
+                } catch (Throwable e) {
+                    callbackHandler.onFailure(new Error(e));
+                }
+            }
+        });
+    }
+}
