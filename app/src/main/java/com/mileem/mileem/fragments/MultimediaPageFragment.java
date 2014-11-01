7342ad3b6a63e743bcbc79c0287b6f789a6640b1
Ramiro Diaz <ramiro.diaz@avature.net>
2014-11-01 14:54:56 -0300

    US 212 - T316 - Actualizo acceso al video.

--- app/src/main/java/com/mileem/mileem/fragments/MultimediaPageFragment.java
+++ app/src/main/java/com/mileem/mileem/fragments/MultimediaPageFragment.java
@@ -1,22 +1,15 @@
 package com.mileem.mileem.fragments;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
 import com.mileem.mileem.AppController;
@@ -60,26 +53,14 @@ public class MultimediaPageFragment extends Fragment {
         networkImageView.setDefaultImageResId(R.drawable.image_placeholder);
         networkImageView.setErrorImageResId(R.drawable.image_placeholder);
         networkImageView.setImageUrl(multimedia.getPreviewUrl(), imageLoader);
-
         networkImageView.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String type = multimedia.getType().toString();
-
                 if (multimedia.getType() == Multimedia.Type.VIDEO) {
-
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://player.vimeo.com/video/" + "106827149")));
-
-
-                    VideoFragment f = VideoFragment.newInstance("XeR0qo0aoXA");
-                    FragmentManager fm = getFragmentManager();
-                    FragmentTransaction tx = fm.beginTransaction();
-                    tx.add(R.id.frame_container, f).addToBackStack( "addToBackStack" ).commit();
-
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(multimedia.getUrl()));
+                    startActivity(intent);
                 }
-                Toast.makeText(v.getContext(), "Multimedia. Tipo: " + type, Toast.LENGTH_LONG).show();
             }
         });
-
 
         return rootView;
     }
