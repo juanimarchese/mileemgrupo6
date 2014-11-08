a9c117a19a4d6d33951d1027d5b1b7c61f063944
JuanMarchese <juan071087>
2014-11-08 17:27:49 -0300

    #Us 219 / TASK#251  Conectividad con el servidor para obtener el gráfico.

--- app/src/main/java/com/mileem/mileem/networking/ReportDataManager.java
+++ app/src/main/java/com/mileem/mileem/networking/ReportDataManager.java
@@ -38,4 +38,26 @@ public class ReportDataManager {
             }
         });
     }
+
+    public void getReportAveragePropertyByEnviromentNeighborhood(final int neighborhoodId, final int width, final int height, final ReportDataManagerCallbackHandler callbackHandler) throws JSONException {
+        RequestParams params = new RequestParams();
+        params.put("neighborhood", neighborhoodId);
+        params.put("width", width);
+        params.put("height", height);
+        AsyncRestHttpClient.get("average-property-by-environment", params, new MileenJsonResponseHandler(callbackHandler) {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    JSONObject payload = response.getJSONObject("payload");
+                    String relativeUrl = payload.getString("url");
+                    String neighborhoodName = payload.getJSONObject("neighborhood").getString("name");
+                    String graphUrl = AsyncRestHttpClient.getAbsoluteUrlRelativeToHost(relativeUrl);
+                    callbackHandler.onComplete(neighborhoodName, graphUrl);
+                } catch (Throwable e) {
+                    callbackHandler.onFailure(new Error(e));
+                }
+            }
+        });
+    }
 }
